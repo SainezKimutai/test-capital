1e6c8228099336b69af270f0acd6d91dd2301bd1
laidbackpeter <ndegwa.muchina3@gmail.com>
2022-09-11 15:01:56 +0300

    Add select2

--- staticfiles/assets/functions/tables/tables.js
+++ staticfiles/assets/functions/tables/tables.js
@@ -10,3 +10,17 @@ $(function () {
       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
   });
+
+
+$(function () {
+    $('#reportTable').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": false,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#reportTable_wrapper .col-md-6:eq(0)');
+  });

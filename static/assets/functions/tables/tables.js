c24c411e7c5bc9cc6539f70122636359a1f5f023
SainezKimutai <sainezkimutai@gmail.com>
2022-10-16 15:15:58 +0300

    Replenishment Payment Update

--- static/assets/functions/tables/tables.js
+++ static/assets/functions/tables/tables.js
@@ -1,12 +1,12 @@
 $(function () {
-    $('.table').DataTable({
-      "paging": false,
+    $('.table-js').DataTable({
+      "paging": true,
       "lengthChange": false,
       "searching": false,
-      "ordering": false,
+      "ordering": true,
       "info": true,
       "autoWidth": false,
       "responsive": true,
       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    }).buttons().container().appendTo('#reports_table_wrapper .col-md-6:eq(0)');
   });
